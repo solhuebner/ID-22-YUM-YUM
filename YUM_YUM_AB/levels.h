43704e911bad38d2cdea8aee449d02c4e2e4c344
Joeri Gantois <info@JO3RI.be>
2016-05-10 12:14:59 +0200

    adding in first little map

--- YUM_YUM_AB/levels.h
+++ YUM_YUM_AB/levels.h
@@ -6,22 +6,12 @@
 
 
 
+
+
 void drawMap()
 {
   // FLOOR TILE
-  sprites.drawPlusMask(16, 8, floorTile_plus_mask, 0);
-
-  // WALL UPPER LEFT
-  sprites.drawPlusMask(64, 8, wall_plus_mask, 0);  
-
-  // WALL UPPER RIGHT
-  sprites.drawPlusMask(96, 8, wall_plus_mask, 1);  
-
-  // WALL LOWER LEFT
-  sprites.drawPlusMask(64, 32, wall_plus_mask, 2);  
-
-  // WALL LOWER RIGHT
-  sprites.drawPlusMask(96, 32, wall_plus_mask, 3);  
+  sprites.drawPlusMask(0, 0, wallTiles_plus_mask, 0);
 }
 
 #endif
